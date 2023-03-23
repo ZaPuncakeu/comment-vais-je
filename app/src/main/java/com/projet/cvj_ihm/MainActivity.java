82b66b9dff62541058175c940086ff7eb65a8822
ZaPuncakeu <anisrouane2@gmail.com>
2023-03-23 05:42:51 -0800

    First commit

---
+++ app/src/main/java/com/projet/cvj_ihm/MainActivity.java
@@ -0,0 +1,20 @@
+package com.projet.cvj_ihm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void start(View v) {
+        Intent intent = new Intent(this, PersonalActivity.class);
+        startActivity(intent);
+    }
+}
